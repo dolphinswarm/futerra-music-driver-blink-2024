f16015729faf816707784c516378643584614949
Brad Schmitz <dolphinswarmentertainment@gmail.com>
2024-07-08 09:28:16 -0600

    Updating scripts

--- python_scripts/music_driver.py
+++ python_scripts/music_driver.py
@@ -38,8 +38,21 @@ mood_picker = op('mood_picker')
 
 # ========= Helper Funcs
 def adjust_chord_to_scale_mode(chord_notes, scale_mode_notes):
-	print('hello')
-	return
+	# Convert input lists from strings to integers
+	chord_notes = [int(note) for note in chord_notes]
+	scale_mode_notes = [int(note) for note in scale_mode_notes]
+
+	# Check each note and adjust it to the proper key / scale mode
+	new_notes = chord_notes[:]
+	for i, note in enumerate(new_notes):
+		# If the note is not in the scale mode, subtract 1 to adjust it for the key (I think? TODO verify?)
+		# Also, we don't want to adjust any 4th note in a scale (TODO maybe find a better system?)
+		if note % 12 not in scale_mode_notes or i != 4:
+			note = note - 1
+
+	# Convert the result back to strings
+	new_notes = [str(note) for note in new_notes]
+	return new_notes
 
 def find_closest_note(note, target_notes):	
     # Find the closest note in target_notes to the given note, considering both current and previous octaves
@@ -59,6 +72,59 @@ def adjust_octave(note, reference):
         note -= 12
     return note
 
+# TODO MORE MELODIES
+def trigger_melody(melody_instruments, chord, scale_mode):
+	# For each melody instrument:
+	for instrument_name, instrument_props in melody_instruments.items():
+		# Get the notes and the base scale
+		scale_notes = scale_notes_table[scale_notes_table.findCell(scale_mode, caseSensitive=True).row, 'Notes'].val.split(',')
+		chord_base_note = min([int(x) for x in chord_table[chord_table.findCell(chord, caseSensitive=True).row, 'Notes Above Root'].val.split(',')])
+		base_note = instrument_props.base_note
+
+		# Set the notes to play for the melody (I, V, VI, IV, V for now) TODO MORE MELODIES
+		notes = (
+			(int(base_note) + int(chord_base_note) + int(scale_notes[0]), 0.0, 1.0, 100, 0), 
+			(int(base_note) + int(chord_base_note) + int(scale_notes[4]), 1.0, 1.0, 100, 0), 
+			(int(base_note) + int(chord_base_note) + int(scale_notes[5]), 2.0, 1.0, 100, 0), 
+			(int(base_note) + int(chord_base_note) + int(scale_notes[3]), 3.0, 1.0, 100, 0),
+			(int(base_note) + int(chord_base_note) + int(scale_notes[4]), 4.0, 3.0, 100, 0), 
+		)
+
+		# Remove all existing notes
+		op(instrument_name).RemoveNotes(timeStart=0, pitchStart=0, timeEnd=8, pitchEnd=127)
+		# op(instrument_name).par.Stopclip.pulse()
+
+		# Play the notes for the new melody
+		op(instrument_name).SetNotes(notes=notes)
+		op(instrument_name).par.Fireclip.pulse()
+
+
+
+def generate_chord_variant(chord, chord_variation, scale_mode, grab_random_variant = False):
+	# Choose a chord variant based on the specified parameter
+	if grab_random_variant == True:
+		# Choose a random chord variation
+		new_variation_row = chord_variations_table.cell(random.randint(1, chord_variations_table.numRows - 1), 0).row
+	else:
+		# Grab a transition chord variation
+		possible_transitions = chord_variations_table[chord_variations_table.findCell(chord_variation, caseSensitive=True).row, 'Possible Transition Variations'].val.split(',')
+		new_variation_row = chord_variations_table.findCell(random.choice(possible_transitions), caseSensitive=True).row
+	
+	new_variation = chord_variations_table[new_variation_row, 'Chord Variation'].val
+	new_variation_notes = chord_variations_table[new_variation_row, 'Notes Above Root'].val.split(',')
+	
+	# Adjust the notes to be in the given scale
+	scale_notes = scale_notes_table[scale_notes_table.findCell(scale_mode, caseSensitive=True).row, 'Notes'].val.split(',')
+	adjust_chord_to_scale_mode(chord_notes=new_variation_notes, scale_mode_notes=scale_notes)
+
+	# Update the global storage variables
+	storage.store('chord_variation', new_variation)
+	chord_base_note = min(chord_table[chord_table.findCell(chord, caseSensitive=True).row, 'Notes Above Root'].val.split(','))
+
+	# Return the new notes for the chord and the chord variation name
+	new_notes = [str(int(x) + int(chord_base_note)) for x in new_variation_notes]
+	return new_notes, chord_variation
+
 def change_notes(current_chord_notes, new_chord_notes, instruments):
 	# For each instrument:
 	new_instruments = {}
@@ -70,7 +136,16 @@ def change_notes(current_chord_notes, new_chord_notes, instruments):
 		# If a bass, just switch to the root note
 		if instrument_props.instrument_role == 'bass':
 			new_chord = normalize_notes([min([int(note) for note in new_chord_notes])])
-			print(new_chord)
+		
+		# If it's a melody, we handle that separately, so just add it back to instruments and call it a day
+		elif instrument_props.instrument_role == 'melody':
+			new_instruments[instrument_name] = Instrument(
+				base_note=instrument_props.base_note,
+				num_voices=0,
+				instrument_role='melody',
+				playing_notes=[]
+			)
+			continue
 
 		# Else, we actually care about notes
 		else:
@@ -125,6 +200,11 @@ def onOffToOn(channel, sampleIndex, val, prev):
 	new_notes = []
 	instruments = storage.fetch('instruments', {})
 
+	# Turn off all midi melodies (TODO BETTER WAY TO DO THIS)
+	melody_instruments = {key: value for key, value in instruments.items() if value.instrument_role == 'melody'}
+	for instrument in melody_instruments:
+		op(instrument).par.Stopclip.pulse()
+
 	# If we should change keys...
 	if key_change_driver['pulse'] == 0:
 		# Reset the value of the OP
@@ -132,7 +212,7 @@ def onOffToOn(channel, sampleIndex, val, prev):
 		key_change_driver.par.resetpulse.pulse()
 
 		# Grab a new key and scale mode, based on the mood and key change limitations
-		current_key_row = keys_table.findCell(key).row
+		current_key_row = keys_table.findCell(key, caseSensitive=True).row
 		new_key = keys_table[current_key_row, 'Common Key Change Keys'].val.split(',')[random.randint(0, 3)] #TODO LOOK AT MORE KEY CHANGES
 		# Update the global storage and local variables
 		storage.store('key', new_key)
@@ -140,16 +220,15 @@ def onOffToOn(channel, sampleIndex, val, prev):
 
 		mood = mood_picker[0, 0].val
 		scale_notes_possible_rows = scale_notes_table.findCells(mood, cols=['Mood'])
-		# scale_notes_row = random.randint(1, 7) <- This is for entirely random scale mode picking
-		scale_notes_row = random.choice(scale_notes_possible_rows).row
+		scale_notes_row = random.choice(scale_notes_possible_rows).row # random.randint(1, 7) <- This is for entirely random scale mode picking
 		new_scale_mode = scale_notes_table[scale_notes_row, 'Name'].val
 		# Update the global storage and local variables
 		storage.store('scale_mode', new_scale_mode)
 		scale_mode = new_scale_mode
 
 		# Grab the new notes of the I chord in the new key
-		scale_notes = scale_notes_table[scale_notes_table.findCell(scale_mode).row, 'Notes'].val.split(',')
-		new_notes = [scale_notes[0], scale_notes[2], scale_notes[4]]
+		scale_notes = scale_notes_table[scale_notes_table.findCell(scale_mode, caseSensitive=True).row, 'Notes'].val.split(',')
+		new_notes = [scale_notes[0], scale_notes[2], scale_notes[4]] # Always I, but intended to that way
 		# Update the global storage and local variables
 		storage.store('chord_variation', 'major triad') # Technically not major for all keys but OH WHALEEEEE (also TODO look at?)
 		chord_variation = 'major triad'
@@ -173,30 +252,33 @@ def onOffToOn(channel, sampleIndex, val, prev):
 		chord = new_chord
 		new_notes = new_chord_notes
 
-		# Update the global storage and local variables
-		# TODO DELETE
-		storage.store('chord_variation', 'major triad') # Technically not major for all keys but OH WHALEEEEE (also TODO look at?)
-		chord_variation = 'major triad'
+		# Randomly land on a variant of the current chord
+		should_do_variant = random.randint(0, 1) == 0
+		if should_do_variant:
+			# Grab a transition chord variation
+			new_chord_variant_info = generate_chord_variant(
+				chord=chord,
+				chord_variation=chord_variation,
+				scale_mode=scale_mode,
+				grab_random_variant=True
+			)
+			new_notes = new_chord_variant_info[0]
+			chord_variation = new_chord_variant_info[1]
+		else:
+			storage.store('chord_variation', 'major triad') # Technically not major for all keys but OH WHALEEEEE (also TODO look at?)
+			chord_variation = 'major triad'
 		
-		# TODO Randomly decide on a new variant
-		# should_do_variant = random.randint(0, 1) == 0
-		# if should_do_variant:
-	
 	# Else, shift to a random variation
 	else:
 		# Grab a transition chord variation
-		possible_transitions = chord_variations_table[chord_variations_table.findCell(chord_variation).row, 'Possible Transition Variations'].val.split(',')
-		new_variation_row = chord_variations_table.findCell(random.choice(possible_transitions)).row
-		new_variation = chord_variations_table[new_variation_row, 'Chord Variation'].val
-		new_variation_notes = chord_variations_table[new_variation_row, 'Notes Above Root'].val.split(',')
-
-		# TODO ADJUST FOR KEY
-
-		# Update the global storage and local variables
-		storage.store('chord_variation', new_variation)
-		chord_variation = new_variation
-		chord_base_note = min(chord_table[chord_table.findCell(chord).row, 'Notes Above Root'].val.split(','))
-		new_notes = [str(int(x) + int(chord_base_note)) for x in new_variation_notes]
+		new_chord_variant_info = generate_chord_variant(
+			chord=chord,
+			chord_variation=chord_variation,
+			scale_mode=scale_mode,
+		)
+		new_notes = new_chord_variant_info[0]
+		chord_variation = new_chord_variant_info[1]
+	
 
 	# Handle changing notes based on updated information
 	change_notes(
@@ -205,9 +287,16 @@ def onOffToOn(channel, sampleIndex, val, prev):
 		instruments=instruments
 	)
 
+	# Trigger the melody for a given chord change or whatever
+
+	trigger_melody(
+		melody_instruments=melody_instruments,
+		chord=chord,
+		scale_mode=scale_mode
+	)
+
 	# Update the new variant + notes after the transition happens
 	storage.store('current_chord_notes', new_notes)
-
 
 	return
 
--- python_scripts/oh_shit.py
+++ python_scripts/oh_shit.py
@@ -8,14 +8,25 @@
 # Make sure the corresponding toggle is enabled in the CHOP Execute DAT.
 
 storage = op('storage_op')
+event_driver = op('event_driver')
 
 def onOffToOn(channel, sampleIndex, val, prev):
+	# Reset the event driver (which tracks global time)
+	event_driver.par.resetpulse.pulse()
+
 	# This script just kills all running instruments LOL
-	# Need to filter to remove the title
 	instruments = storage.fetch('instruments', {})
-	for instrument in instruments:
-		op(instrument).SendMIDI('flush')
-		op(instrument).par.Clearchop.pulse()
+	for instrument_name, instrument_props in instruments.items():
+		# If a melody, reset its clip
+		if instrument_props.instrument_role == 'melody':
+			# Remove all existing notes
+			op(instrument_name).RemoveNotes(timeStart=0, pitchStart=0, timeEnd=8, pitchEnd=127)
+			op(instrument_name).par.Stopclip.pulse()
+
+		# Else, it's MIDI, so reset it
+		else:
+			op(instrument_name).SendMIDI('flush')
+			op(instrument_name).par.Clearchop.pulse()
 
 	return
 
